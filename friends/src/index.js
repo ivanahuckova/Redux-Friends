cb38d81b664e06195189866d28d61f5e05be0c9e
Ivana Huckova <ivana.huckova@gmail.com>
2019-03-07 14:02:16 +0100

    Reducer and store basic set up

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import { logger } from 'redux-logger';
+import { thunk } from 'redux-thunk';
 import './index.css';
 import App from './App';
+import { friends } from './reducers/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  friends,
+  compose(
+    applyMiddleware(logger, thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
---
+++ friends/src/reducers/index.js
@@ -0,0 +1,10 @@
+const initialState = {
+  friends: []
+};
+
+export function friends(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
