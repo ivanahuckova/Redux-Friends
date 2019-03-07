68cb4def6fe4724b0b984d1b07e0c763e0b42e0c
Ivana Huckova <ivana.huckova@gmail.com>
2019-03-07 14:25:01 +0100

    Create navbar

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
 
-import LoginPage from './components/LoginPage';
+import Login from './components/Login';
+import Navbar from './components/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <LoginPage />
+        <Navbar />
+        <Login />
       </div>
     );
   }
